b1bd310e801e2ea522447737c7972c76eefd1931
Katerina Tang <kbt28@cornell.edu>
2024-01-29 12:26:26 -0500

    Add scripts for parallel example on remote machine

---
+++ main_parallel_remote.jl
@@ -0,0 +1,58 @@
+#for use on remote machine
+
+using Distributed
+
+num_cores = parse(Int,ENV["SLURM_TASKS_PER_NODE"])
+addprocs(num_cores)
+
+# instantiate and precompile environment
+@everywhere begin
+  using Pkg;Pkg.activate(@__DIR__); 
+  Pkg.instantiate(); Pkg.precompile()
+end
+
+# load dependencies and helper functions
+@everywhere begin
+  using ProgressMeter
+  using CSV
+
+  function process(infile, outfile)
+    # read file from disk
+    csv = CSV.File(infile)
+
+    # perform calculations
+    sleep(60)
+
+    # save new file to disk
+    CSV.write(outfile, csv)
+  end
+end
+
+# MAIN SCRIPT
+# -----------
+
+# relevant directories
+indir  = joinpath(@__DIR__,"data")
+outdir = joinpath(@__DIR__,"results")
+
+# files to process
+infiles  = readdir(indir, join=true)
+outfiles = joinpath.(outdir, basename.(infiles))
+nfiles   = length(infiles)
+
+# process files in parallel
+status = @showprogress pmap(1:nfiles) do i
+  try
+    process(infiles[i], outfiles[i])
+    true # success
+  catch e
+    false # failure
+  end
+end
+
+# report files that failed
+failed = infiles[.!status]
+if !isempty(failed)
+  println("List of files that failed:")
+  foreach(println, failed)
+end
---
+++ main_parallel_remote.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=parallel_test_julia
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --output=results/output_text.txt
+#SBATCH --error=results/error_text.txt
+#SBATCH --exclusive
+
+# Print properties of job as submitted
+echo "SLURM_JOB_ID = $SLURM_JOB_ID"
+echo "SLURM_NTASKS = $SLURM_NTASKS"
+echo "SLURM_NTASKS_PER_NODE = $SLURM_NTASKS_PER_NODE"
+echo "SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK"
+echo "SLURM_JOB_NUM_NODES = $SLURM_JOB_NUM_NODES"
+
+# Print properties of job as scheduled by Slurm
+echo "SLURM_JOB_NODELIST = $SLURM_JOB_NODELIST"
+echo "SLURM_TASKS_PER_NODE = $SLURM_TASKS_PER_NODE"
+echo "SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE"
+echo "SLURM_CPUS_ON_NODE = $SLURM_CPUS_ON_NODE"
+
+# Run the Julia code (specify version)
+julia +1.9.0 main.jl
