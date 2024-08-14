7dfce54ff235ae5a3587fdbae29d5a925c81e664
Katerina Tang <kbt28@cornell.edu>
2024-08-14 16:57:11 -0400

    add shell script

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=test_script
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --output=results/out-%j.txt
+#SBATCH --error=results/err-%j.txt
+
+# Print properties of job as submitted
+echo "SLURM_JOB_ID = $SLURM_JOB_ID"
+echo "SLURM_NTASKS = $SLURM_NTASKS"
+echo "SLURM_NTASKS_PER_NODE = $SLURM_NTASKS_PER_NODE"
+echo "SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK"
+echo "SLURM_JOB_NUM_NODES = $SLURM_JOB_NUM_NODES"
+
+# Print properties of job as scheduled by Slurm
+echo "SLURM_JOB_NODELIST = $SLURM_JOB_NODELIST"
+echo "SLURM_TASKS_PER_NODE = $SLURM_TASKS_PER_NODE"
+echo "SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE"
+echo "SLURM_CPUS_ON_NODE = $SLURM_CPUS_ON_NODE"
+
+# Run the Julia code (specify Julia version with juliaup)
+julia +1.9.0 main.jl
+
+exit 0
